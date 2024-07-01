f82911f1f50791fca47f0b563bc8571123b91f8c
guiisbarbosa <gui.silva93@hotmail.com>
2024-07-01 12:00:40 -0300

    fix(Card) alteração nas props, para ter valores padrões

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import * as S from '../styles/cardStyle'
 
 import BlackStripes from '/black-stripes.svg'
 
-function Card({ pokeImg, pokeName, pokeID }) {
+function Card({ pokeImg, pokeName = '', pokeID = '' }) {
 
     function capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
@@ -14,7 +14,7 @@ function Card({ pokeImg, pokeName, pokeID }) {
                 <div className="redDot" style={{ width: 6, height: 6, margin: '0 8px' }}></div>
                 <div className="redDot" style={{ width: 6, height: 6, margin: '0 8px' }}></div>
             </div>
-            <div className="cardScreen"><img src={pokeImg} width="200px" /><S.PokemonID>#{pokeID.toString().padStart(4, '0')}</S.PokemonID></div>
+            <div className="cardScreen"><img src={pokeImg} width="200px" />{pokeID !== '' ? <S.PokemonID>#{pokeID.toString().padStart(4, '0')}</S.PokemonID> : ''}</div>
             <div className="cardInfo">
                 <div className="redDot" style={{ width: 24, height: 24, border: '2px solid #000' }}></div>
                 <div><p>{capitalizeFirstLetter(pokeName)}</p></div>
